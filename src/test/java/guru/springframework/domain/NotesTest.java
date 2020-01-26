af4ba78b9b9b2e431a50c11c98969037672de31a
RISHABH <rishi.1803@gmail.com>
2020-01-26 14:59:48 +0530

    juint 4 to 5 migration

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Rishabh on 26,Jan,2020
+ */
+class NotesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals(1,1);
+    }
+}
